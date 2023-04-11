5ddd83cbfd13ab7d9aacddf91ef2bf7e44f5c4fb
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-11 16:58:54 +0100

    server is servering

--- controller/usercontroller.ts
+++ controller/usercontroller.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response } from "express"
 import usermodel from "../model/usermodel"
 import crypto from "crypto"
+import { verifyAccount } from "../util/email"
 
 export const userregister = async (req: Request, res: Response)=> {
     try {
         const { userName, email, password, token, verified, otp } = req.body
         const getToken = await crypto.randomBytes(32)
         const getOtp = await crypto.randomBytes(2)
+
+        
         
         const user = await usermodel.create({
             userName,
@@ -16,6 +19,15 @@ export const userregister = async (req: Request, res: Response)=> {
             verified,
             otp: getOtp
         })
+
+        verifyAccount(user)
+            .then((res) => {
+                console.log("email server is created")
+            })
+            .catch((res) => {
+            console.log("An error occured")
+        })
+        
 
         return res.status(200).json({
             message: "User created successfully",
--- util/email.ts
+++ util/email.ts
@@ -3,7 +3,7 @@ import { google } from "googleapis"
 
 const GOOGLE_ID: string = "1008024411990-3hl9ed3v8bmdvh9706h81178ret30mpi.apps.googleusercontent.com";
 const GOOGLE_SECRET: string = "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";
-const GOOGLE_REFRESHTOKEN: string = "1//04wbDW-p9NMXaCgYIARAAGAQSNwF-L9IryEJOwQnS96dOBiDBsB_5mln_tNDYNZdP1iQF93rEGSNz5n--4qDuQBd2T9rcrWfqoRk";
+const GOOGLE_REFRESHTOKEN: string = "1//04JWXUxOMgmMcCgYIARAAGAQSNwF-L9IrG_08kawbfgJ-3nTJuK2H5kfsaCoQ-h0fQxjxp2m6df6e4Mic0yZyJs6Qk8dGG0wyDvw";
 const GOOGLE_REDIRECT: string = "https://developers.google.com/oauthplayground/";
 
 const oAuth = new google.auth.OAuth2(GOOGLE_ID, GOOGLE_SECRET, GOOGLE_REDIRECT)
@@ -18,15 +18,16 @@ export const verifyAccount = async (user: any) => {
             service: "gmail",
             auth: {
                 type: "OAUTH2",
+                user: "okwolig60@gmail.com",
                 clientId: GOOGLE_ID,
                 clientSecret: GOOGLE_SECRET,
                 refreshToken: GOOGLE_REFRESHTOKEN,
-                accessToken: "ya29.a0Ael9sCM7AziXiNhmk3Fq3rS9DLdhlgE12a6cyhGPNzaqFHeR5Zsxxl1yhp1YH3sKTKmXyoJe4Qiz1hrss5yYWLLERXngVV6T3wYDDufVzCjj84xk9_ul4Ab1QOpDJ4IqyQ_r9wyGCPsn4qVUggdqXkzWdV2TaCgYKARcSARISFQF4udJh2ZpV4JLnSP8OIlOZ70xXTQ0163"
+                accessToken: "ya29.a0Ael9sCMVmDHO0AksTvP0kHRQnp4jcy2IeYM2rO50QFcF7O4eIMLUyWqbaZhkN5J3CjPzfxWbt805AWoUEgkw69-KWJJHOjU_MPi9vEY51gTurTayXtFr0eZtjPp3IOdv0PeLb8-BYiq4bg4nnAiiCGUpCKHKaCgYKAf4SARISFQF4udJh5lHKvTa9LURx0Vvfo1Vjww0163"
             }
         })
 
         const mailOptions = {
-            from: "MyMail<okwolig60@gmail.com>",
+            from: "MyMail <okwolig60@gmail.com>",
             to: user.email,
             subject: "Account Verification",
             html: `<div>Welcome to easyHr platform ${user.userName}</div>`
