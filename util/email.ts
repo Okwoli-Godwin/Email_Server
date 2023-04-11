201c72a7cb6af056176ffef4e5c430dcc41f3a52
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-11 15:43:38 +0100

    server is servering

--- util/email.ts
+++ util/email.ts
@@ -3,8 +3,8 @@ import { google } from "googleapis"
 
 const GOOGLE_ID: string = "1008024411990-3hl9ed3v8bmdvh9706h81178ret30mpi.apps.googleusercontent.com";
 const GOOGLE_SECRET: string = "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";
-const GOOGLE_REFRESHTOKEN: string = "";
-const GOOGLE_REDIRECT: string = "";
+const GOOGLE_REFRESHTOKEN: string = "ya29.a0Ael9sCM7AziXiNhmk3Fq3rS9DLdhlgE12a6cyhGPNzaqFHeR5Zsxxl1yhp1YH3sKTKmXyoJe4Qiz1hrss5yYWLLERXngVV6T3wYDDufVzCjj84xk9_ul4Ab1QOpDJ4IqyQ_r9wyGCPsn4qVUggdqXkzWdV2TaCgYKARcSARISFQF4udJh2ZpV4JLnSP8OIlOZ70xXTQ0163";
+const GOOGLE_REDIRECT: string = "https://developers.google.com/oauthplayground/";
 
 const oAuth = new google.auth.OAuth2(GOOGLE_ID, GOOGLE_SECRET, GOOGLE_REDIRECT)
 
